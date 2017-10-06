3adf046a88d97c81411ca57e9fae80400bf992c4
Coopers-Wu <269926183@qq.com>
2017-10-06 11:27:27 +0800

    载入参数

---
+++ tf_restore.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+weights = tf.Variable(tf.zeros([784,200]),name='weights')
+biases = tf.Variable(tf.zeros([200]),name='biases')
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(init)
+    save_path = saver.restore(sess,'mynet/test.ckpt')
+    print('OK~')
